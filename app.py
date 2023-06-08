19d977c50e8b567d89efc46c2e1df8cc26544478
mdsimpson09 <131899003+mdsimpson09@users.noreply.github.com>
2023-06-08 12:47:29 -0400

    initial commit for madlibs- further study assignment not completed on this commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "key"
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def questions():
+    """creates and shows form to ask for words"""
+    prompts = story.prompts
+    return render_template('questions.html', prompts=prompts)
+
+@app.route('/story')
+def show_story():
+
+    text= story.generate(request.args)
+    return render_template('story.html', text=text)
